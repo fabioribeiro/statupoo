39281fb5fb20e7bef6cf6a7453a23da050400e5e
FABIO <FABIO@FABIO-PC>
2015-11-08 23:35:14 -0200

    Iniciando projeto de POO II

--- statupoo/src/java/fvs/edu/usuario/Usuario.java
+++ statupoo/src/java/fvs/edu/usuario/Usuario.java
@@ -21,13 +21,10 @@ public class Usuario {
     private int id;
     
     @Column (length = 150, nullable = true)
-    private String nome;
+    private String usuario;
     
     @Column (length = 150, nullable = true)
-    private String email;
-    
-    @Column (length = 20, nullable = true)
-    private String endereco;
+    private String senha;
     
     @Column (length = 50, nullable = true)
     private String telefone;
@@ -40,36 +37,21 @@ public class Usuario {
         this.id = id;
     }
 
-    public String getNome() {
-        return nome;
+    public String getUsuario() {
+        return usuario;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
     }
 
-    public String getEmail() {
-        return email;
+    public String getSenha() {
+        return senha;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
+    
    
 }
