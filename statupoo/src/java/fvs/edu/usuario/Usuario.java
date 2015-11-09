deadfbc4d3dbc787bc6b83fbd483dc1b81c0f879
FABIO <FABIO@FABIO-PC>
2015-11-08 22:46:00 -0200

    Iniciando projeto de POO II

--- statupoo/src/java/fvs/edu/usuario/Usuario.java
+++ statupoo/src/java/fvs/edu/usuario/Usuario.java
@@ -5,10 +5,71 @@
  */
 package fvs.edu.usuario;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  *
  * @author FABIO
  */
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue
+    private int id;
     
+    @Column (length = 150, nullable = true)
+    private String nome;
+    
+    @Column (length = 150, nullable = true)
+    private String email;
+    
+    @Column (length = 20, nullable = true)
+    private String endereco;
+    
+    @Column (length = 50, nullable = true)
+    private String telefone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+   
 }
